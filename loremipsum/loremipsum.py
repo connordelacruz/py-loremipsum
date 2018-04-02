05d6fe2b1167ddb76c7b38d4cbebcacb4d3be302
Connor de la Cruz <cdelacruz@sccadv.com>
2018-04-02 15:33:54 -0500

    Modified generate() to handle html options
    - Removed generate_html() method as it seemed redundant to implement
    - Added and implemented private methods _request_url_string() and
    _generate(). These handle building the request url and making the
    request
    - Added generate() parameter html_options which takes a list of strings
    for valid html options and adds those to the request URL. Invalid items
    are not added, and param is ignored entirely if plaintext=True

--- loremipsum/loremipsum.py
+++ loremipsum/loremipsum.py
@@ -15,43 +15,7 @@ class LoremIpsum(object):
         LONG = 'long'
         VERY_LONG = 'verylong'
 
-    # TODO: make this method always be plaintext, add method for HTML with different params
-    @staticmethod
-    def generate(paragraph_count=None, paragraph_length=None, plaintext=True, allcaps=False, prude=False):
-        """Generate Lorem Ipsum placeholder text using the https://loripsum.net API.
-
-        Further documentation of parameters can be found at `loripsum.net <https://loripsum.net>`_
-
-        :param paragraph_count: (Optional) The number of paragraphs to generate. If
-            unspecified, API defaults to 3
-        :param paragraph_length: (Optional) The average length of a paragraph. Possible
-            values are declared as attributes in ``LoremIpsum.ParagraphLength``. If
-            unspecified, API defaults to 'long'
-        :param plaintext: (Default = True) Return plain text, no HTML
-        :param allcaps: (Default = False) Use ALL CAPS
-        :param prude: (Default = False) Prude version. From the API documentation:
-            "The original text contains a few instances of words like 'sex' or 'homo'.
-            Personally, we don't mind, because these are just common latin words
-            meaning 'six' and 'man'. However, some people (or your clients) might be
-            offended by this, so if you select the 'Prude version', these words will be
-            censored."
-        """
-        global URL
-        request_url = URL
-        if paragraph_count is not None:
-            request_url += '{}/'.format(paragraph_count)
-        if paragraph_length:
-            request_url += paragraph_length + '/'
-        if plaintext:
-            request_url += 'plaintext/'
-        if allcaps:
-            request_url += 'allcaps/'
-        if prude:
-            request_url += 'prude/'
-        placeholder_text = request.urlopen(request_url).read().decode('utf8')
-        return placeholder_text
-
-    # Valid keys for html_kwargs
+    # Valid keys for html_options
     HTML_OPTIONS = [
         'decorate', # Add bold, italic and marked text.
         'link', # Add links.
@@ -64,8 +28,68 @@ class LoremIpsum(object):
     ]
 
     @staticmethod
-    def generate_html(paragraph_count=None, paragraph_length=None, allcaps=False, prude=False, **html_kwargs):
-        # TODO: document html_kwargs
-        # TODO: implement
-        pass
+    def _request_url_string(request_args):
+        global URL
+        url_string = URL
+        for arg in request_args:
+            url_string += '{}/'.format(arg)
+        return url_string
+
+    @staticmethod
+    def _generate(request_url):
+        placeholder_text = request.urlopen(request_url).read().decode('utf8')
+        return placeholder_text
+
+    @staticmethod
+    def generate(paragraph_count=None, paragraph_length=None, allcaps=False, prude=False, plaintext=True, html_options=None):
+        """Generate Lorem Ipsum placeholder text using the https://loripsum.net API.
+
+        Further documentation of parameters can be found at `loripsum.net <https://loripsum.net>`_
+
+        :param paragraph_count: (Optional) The number of paragraphs to generate. If
+            unspecified, API defaults to 3
+        :param paragraph_length: (Optional) The average length of a paragraph. Possible
+            values are declared as attributes in ``LoremIpsum.ParagraphLength``. If
+            unspecified, API defaults to 'long'
+        :param allcaps: (Default = False) Use ALL CAPS
+        :param prude: (Default = False) Prude version. From the API documentation:
+            "The original text contains a few instances of words like 'sex' or 'homo'.
+            Personally, we don't mind, because these are just common latin words
+            meaning 'six' and 'man'. However, some people (or your clients) might be
+            offended by this, so if you select the 'Prude version', these words will be
+            censored."
+        :param plaintext: (Default = True) Return plain text, no HTML
+        :param html_options: (Default = None) List of html options to specify in
+            request. This will be ignored if plaintext = True. The following options
+            are accepted:
+
+                - 'decorate' - Add bold, italic and marked text.
+		- 'link' - Add links.
+                - 'ul' - Add unordered lists.
+                - 'ol' - Add numbered lists.
+                - 'dl' - Add description lists.
+                - 'bq' - Add blockquotes.
+                - 'code' - Add code samples.
+                - 'headers' - Add headers.
+        """
+        request_args = []
+        if paragraph_count is not None:
+            request_args.append(paragraph_count)
+        if paragraph_length:
+            request_args.append(paragraph_length)
+        if allcaps:
+            request_args.append('allcaps')
+        if prude:
+            request_args.append('prude')
+        if plaintext:
+            request_args.append('plaintext')
+        # If not plaintext and html_options is specified, add those args as well
+        elif html_options is not None:
+            # Ignore invalid options
+            valid_html_options = [
+                option for option in html_options if option in LoremIpsum.HTML_OPTIONS
+            ]
+            request_args.extend(valid_html_options)
+        request_url = LoremIpsum._request_url_string(request_args)
+        return LoremIpsum._generate(request_url)
 
